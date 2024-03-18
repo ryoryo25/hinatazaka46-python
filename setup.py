f6f59d40dd0facf8450979c033c95484fd93dba7
ry_Tom <ryotoyota@outlook.jp>
2024-03-18 15:39:42 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hinatazaka46-python",
+    version="1.0",
+    description="utilities for developing tools of hinatazaka46",
+    author="ry_Tom",
+    packages=find_packages(),
+    license="MIT"
+)
