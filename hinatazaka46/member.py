342c01afbd51091ad9dea799079e77835b2673ab
ry_Tom <ryotoyota@outlook.jp>
2024-08-10 00:29:59 +0900

    handle Poka

--- hinatazaka46/member.py
+++ hinatazaka46/member.py
@@ -10,8 +10,12 @@ class Name:
     def __init__(self, ja_full: str, en_full: str) -> None:
         ja_split = ja_full.split()
         en_split = en_full.split()
-        self._family = (ja_split[0], en_split[1])
-        self._given = (ja_split[1], en_split[0])
+        if len(ja_split) == 2:
+            self._family = (ja_split[0], en_split[1])
+            self._given = (ja_split[1], en_split[0])
+        else: # for Poka
+            self._family = (None, None)
+            self._given = (ja_split[0], en_split[0])
 
     def get_full_name(self, en: bool = False, case: StringCase = StringCase.Plain) -> str:
         name_list = self.get_full_name_as_list(en)
@@ -30,10 +34,15 @@ class Name:
         return sep.join(name_list)
 
     def get_full_name_as_list(self, en: bool = False) -> list[str]:
-        result = [self.get_family_name(en), self.get_given_name(en)]
+        family = self.get_family_name(en)
+        given = self.get_given_name(en)
+        if family is None:
+            result = [given]
+        else:
+            result = [family, given]
         return reversed(result) if en else result
 
-    def get_family_name(self, en: bool = False) -> str:
+    def get_family_name(self, en: bool = False) -> str | None:
         return self._family[int(en)]
 
     def get_given_name(self, en: bool = False) -> str:
